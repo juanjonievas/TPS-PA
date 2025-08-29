fedc96fc3866d6d7215b0d99e9d1f04a2b45fe7a
UADE_IS2_Nievas <167822929+juanjonievas@users.noreply.github.com>
2025-08-29 19:58:54 -0300

    Update Ejercicio1.js

--- Redesdedatos/Ejercicio1.js
+++ Redesdedatos/Ejercicio1.js
@@ -1,11 +1,12 @@
 //Crear un objeto literal para un dispositivo de red 
 
 const router = {
-  modelo: "",
-  marca: "",
+  modelo: "TL-SG108",
+  marca: "TP-Link",
   puertos: 8,
   velocidad: 6000,  // en Mbps
   soportaWifi: true
 };
+
 
 console.log(router);
